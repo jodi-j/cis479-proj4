6770be6e4cd60cc6257d3da7d990e108b125bd81
jodi <128635337+jodi-j@users.noreply.github.com>
2024-04-17 13:48:44 -0400

    queries 2-5 (unsure if correct though)

--- StudentBehaviors.py
+++ StudentBehaviors.py
@@ -43,8 +43,8 @@ graph.render('overdrawn_dt', view=True)
 #Query 1: Predict whether a 20-year-old male student who has drunk alcohol for 10 days in the past 30 
 #         days will overdraw a checking account.
 query_1_data = pd.DataFrame({'Age': [20],
-                             'Sex': [0],  # Male
-                             'DaysDrink': [1]})  # Drunk alcohol for 10 days
+                             'Sex': [0], 
+                             'DaysDrink': [1]}) 
 prediction_1 = dtree.predict(query_1_data)
 if (prediction_1 == [1]):
     pred_1_text = "Yes"
@@ -54,16 +54,48 @@ print("Prediction 1: Will the student overdraw a checking account?", pred_1_text
 
 #Query 2:Predict whether a 25-year-old female student who has drunk alcohol for 5 days in the past 30 
 #         days will overdraw a checking account.
-print("Prediction 2: Will the student overdraw a checking account?")
+query_2_data = pd.DataFrame({'Age': [25],
+                             'Sex': [1],
+                             'DaysDrink': [0]})
+prediction_2 = dtree.predict(query_2_data)
+if (prediction_2 == [1]):
+    pred_2_text = "Yes"
+elif (prediction_2 == [0]):
+    pred_2_text = "No"
+print("Prediction 2: Will the student overdraw a checking account?", pred_2_text)
 
 #Query 3: Predict whether a 19-year-old male student who has drunk alcohol for 20 days in the past 30 
 #         days will overdraw a checking account.
-print("Prediction 3: Will the student overdraw a checking account?")
+query_3_data = pd.DataFrame({'Age': [19],
+                             'Sex': [0],
+                             'DaysDrink': [2]})
+prediction_3 = dtree.predict(query_3_data)
+if (prediction_3 == [1]):
+    pred_3_text = "Yes"
+elif (prediction_3 == [0]):
+    pred_3_text = "No"
+print("Prediction 3: Will the student overdraw a checking account?", pred_3_text)
 
 #Query 4: Predict whether a 22-year-old female student who has drunk alcohol for 15 days in the past 30 
 #         days will overdraw a checking account.
-print("Prediction 4: Will the student overdraw a checking account?")
+query_4_data = pd.DataFrame({'Age': [22],
+                             'Sex': [1],
+                             'DaysDrink': [2]})
+prediction_4 = dtree.predict(query_4_data)
+if (prediction_4 == [1]):
+    pred_4_text = "Yes"
+elif (prediction_4 == [0]):
+    pred_4_text = "No"
+print("Prediction 4: Will the student overdraw a checking account?", pred_4_text)
 
 #Query 5: Predict whether a 21-year-old male student who has drunk alcohol for 20 days in the past 30 
 #         days will overdraw a checking account.
-print("Prediction 5: Will the student overdraw a checking account?")
+query_5_data = pd.DataFrame({'Age': [21],
+                             'Sex': [0],
+                             'DaysDrink': [2]})
+prediction_5 = dtree.predict(query_5_data)
+if (prediction_5 == [1]):
+    pred_5_text = "Yes"
+elif (prediction_5 == [0]):
+    pred_5_text = "No"
+print("Prediction 5: Will the student overdraw a checking account?", pred_5_text)
